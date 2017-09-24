5ae1bbf81ce02e7dde038fc20bb5021372c77555
duyanh <duyanh2005@gmail.com>
2017-09-24 22:57:58 +0700

    update pages

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ server.set('port', process.env.PORT || 3000);
 // register web app
 var public = path.join(__dirname, 'public');
 server.use('/', express.static(public, { index: 'index.html' }));
+server.use('/index', express.static(public, { index: 'index.html' }));
+server.use('/alternate', express.static(public, { index: 'alternate.html' }));
 
 // export
 module.exports = server;
